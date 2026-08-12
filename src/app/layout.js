4091be3924d6b820096295606988ce9f4691d504
nusratjahan7 <nusratjahan77222@gmail.com>
2026-08-12 13:21:17 +0600

    add betterauth for user login

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       
-      <body className="min-h-full bg-noise flex flex-col">
+      <body className="min-h-full bg-noise fontP flex flex-col">
         <Navbar />
         <main>{children}</main>
         </body>
